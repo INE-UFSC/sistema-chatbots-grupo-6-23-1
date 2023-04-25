a3ee2ca2e98ed3ccae4919be4fd1e94f8dd7487d
Felipe Pacheco <lipe.fagundespacheco@gmail.com>
2023-04-25 10:51:50 -0300

    fix BotZiggs

--- Bots/BotZiggs.py
+++ Bots/BotZiggs.py
@@ -1,8 +1,13 @@
 from Bots.Bot import Bot
 
 class BotZiggs(Bot):
-    def __init__(self, nome, comandos):
-        super().__init__(nome, comandos)
+    def __init__(self, nome):
+        comandos = {"Quem eh voce?": "Eu sou Ziggs",
+                    "Quantos anos voce existe no LoL?": "Eu circulo pelos campos da justiça durante 11 anos, desde 2012",
+                    "Quantas skins voce possui?": "Eu tenho 9 skin dentro de League of Legends, sendo a mais rara a skin Hextec",
+                    "Quem voce mais odeia?":"Eu odeio aquele noxiano chamado Draven!!"}
+        super().__init__(nome,comandos)
+
 
     def apresentacao(self):
         print("Meu nome é" ,self.__nome, "alguem quer jogar?")
