c786e83c16dbfd3f904b260ecd843f3a805bf4b3
Provavelmente-o-Caio <caiofecardoso@hotmail.com>
2023-04-25 10:38:03 -0300

    Mudança de print

--- Bots/BotHogRider.py
+++ Bots/BotHogRider.py
@@ -7,10 +7,10 @@ class BotHogRider(Bot):
         super().__init__(nome, comandos)
 
     def apresentacao(self):
-        return(f"Olá eu sou o {self.__nome} do clash of clans HOG RIDAAAAA, adoro destruir tudo e hoje estou com muita raiva")
+        print(f"Olá eu sou o {self.__nome} do clash of clans HOG RIDAAAAA, adoro destruir tudo e hoje estou com muita raiva")
 
     def boas_vindas(self):
-        return("Não acredito que me escolheu, em minha terra chamam isso de noobice")
+        print("Não acredito que me escolheu, em minha terra chamam isso de noobice")
 
     def despedida(self):
-        return("Até que enfim, já não tava aguentando, HOG RIDAAAA")
+        print("Até que enfim, já não tava aguentando, HOG RIDAAAA")
