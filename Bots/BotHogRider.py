29b596dae28b07fcd3c2fdc646786719ec76d934
André Thiago <andrethiagopfleger@gmail.com>
2023-04-25 10:25:07 -0300

    Alteracoes bot hog ridder

---
+++ Bots/BotHogRider.py
@@ -0,0 +1,16 @@
+from Bots.Bot import Bot
+from abc import ABC, abstractmethod
+import random as r
+
+class BotHogRider(Bot):
+    def __init__(self):
+        pass
+
+    def apresentacao(self):
+        return("Olá eu sou o corredor do clash of clans HOG RIDAAAAA, adoro destruir tudo e hoje estou com muita raiva")
+
+    def boas_vindas(self):
+        return("Não acredito que me escolheu, em minha terra chamam isso de noobice")
+
+    def despedida(self):
+        return("Até que enfim, já não tava aguentando, HOG RIDAAAA")
