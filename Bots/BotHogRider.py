2dea36372c48e55115ad67018fe62638539a8ce6
André Thiago <andrethiagopfleger@gmail.com>
2023-04-25 10:30:38 -0300

    Atualizado

--- Bots/BotHogRider.py
+++ Bots/BotHogRider.py
@@ -3,11 +3,11 @@ from abc import ABC, abstractmethod
 import random as r
 
 class BotHogRider(Bot):
-    def __init__(self):
-        pass
+    def __init__(self, nome, comandos):
+        super().__init__(nome, comandos)
 
     def apresentacao(self):
-        return("Olá eu sou o corredor do clash of clans HOG RIDAAAAA, adoro destruir tudo e hoje estou com muita raiva")
+        return(f"Olá eu sou o {self.__nome} do clash of clans HOG RIDAAAAA, adoro destruir tudo e hoje estou com muita raiva")
 
     def boas_vindas(self):
         return("Não acredito que me escolheu, em minha terra chamam isso de noobice")
