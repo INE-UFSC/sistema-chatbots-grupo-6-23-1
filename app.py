307a6a6de282fdaca9cd88d4be7bf78aa1aefff7
Arthur de Lara Machado <arthurlaramachado@gmail.com>
2023-04-27 12:41:58 -0300

    added env variables and gpt bot instance

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+from py_env import run_env
+
+run_env()
+
 #encoding: utf-8
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotHogRider import BotHogRider
 from Bots.BotZiggs import BotZiggs
+from Bots.BotChatGpt import BotChatGpt
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZiggs("Ziggs"), BotHogRider("Hog Ridder")]
-
+lista_bots = [BotZiggs("Ziggs"), BotHogRider("Hog Ridder"), BotChatGpt("Harry Potter")]
+    
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
---
+++ py_env.py
@@ -0,0 +1,4 @@
+import os
+
+def run_env():
+    os.environ['API_KEY'] = 'your_key_here'
